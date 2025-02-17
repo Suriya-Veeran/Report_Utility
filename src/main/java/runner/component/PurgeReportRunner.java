cf38a63807fc384a2f05018735f7708ae30f4a88
Suriya-Veeran <suriya.veeran@platform3solutions.com>
2025-02-17 23:31:14 +0530

    Runner package added for demo

---
+++ src/main/java/runner/component/PurgeReportRunner.java
@@ -0,0 +1,13 @@
+package runner.component;
+
+import lombok.extern.slf4j.Slf4j;
+import runner.enums.ReportNameConstants;
+import runner.services.CommonRunner;
+
+@Slf4j
+public class PurgeReportRunner implements CommonRunner {
+    @Override
+    public void generateReport(String location, ReportNameConstants reportNameConstants) {
+
+    }
+}
---
+++ src/main/java/runner/services/CommonRunner.java
@@ -0,0 +1,9 @@
+package runner.services;
+
+import runner.enums.ReportNameConstants;
+
+public interface CommonRunner {
+
+    void generateReport(String location, ReportNameConstants reportNameConstants);
+
+}
